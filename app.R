c7dd7a78fb4b9e9f809a3347f6c23bb721c783cc
Andrea herrera <aniaherrera02@gmail.com>
2022-04-18 23:15:28 -0500

    layout

--- app.R
+++ app.R
@@ -16,6 +16,8 @@ library(scales)
 
 alldata <- getAllData()
 
+#placeholder for communities
+communities <- getComm()
 
 # Define UI for application that draws a histogram
 # Define UI for random distribution app ----
@@ -28,47 +30,60 @@ ui <- fluidPage(
   #sidebarLayout(
   fluidRow(column(width = 12,leafletOutput("leaf"))),
     
+  sidebarPanel(width = '2',
+               
+               # Input: Select the random distribution type ----
+               radioButtons("Len", "Distance Conversion:",
+                            c("Kilometers" = "kilo",
+                              "Miles" = "miles")),
+               # br() element to introduce extra vertical spacing ----
+               radioButtons("time", "Time Conversion:",
+                            c("24 Hour" = "24H",
+                              "12 Hour" = "12H"))
+  ),
     # Main panel for displaying outputs ----
     mainPanel(
-      
+     
       # Output: Tabset w/ plot, summary, and table ----
       tabsetPanel(
-        tabPanel("Plot 1",navlistPanel(tabPanel("plot 1",plotOutput("plot")),tabPanel("plot 2",plotOutput("plot2"))),
-                 # Sidebar panel for inputs ----
-                 sidebarPanel(
-                   
-                   # Input: Select the random distribution type ----
-                   radioButtons("dist", "Distribution type:",
-                                c("Normal" = "norm",
-                                  "Uniform" = "unif",
-                                  "Log-normal" = "lnorm",
-                                  "Exponential" = "exp")),
-                   
-                   # br() element to introduce extra vertical spacing ----
-                   br(),
-                   
-                   # Input: Slider for the number of observations to generate ----
-                   sliderInput("n",
-                               "Number of observations:",
-                               value = 500,
-                               min = 1,
-                               max = 1000)
-                   
-                 )   
+
+        tabPanel("All Data",
+                 navlistPanel(widths = c(2,10), tabPanel("Plot 1",fluidRow( splitLayout(cellWidths = c('25%','25%','25%','25%'),plotOutput("plot"),plotOutput("plot2"),plotOutput("plot3"),plotOutput("plot4")))),
+                                                tabPanel("Plot 2",fluidRow( splitLayout(cellWidths = c('50%','50%'),plotOutput("p5"),plotOutput("p6")))),
+                                                tabPanel("Tabular",plotOutput("plotTab")),
+                              )
+        ),
+        tabPanel("Community",
+                 navlistPanel(widths = c(2,10),  tabPanel("HeatMap ",plotOutput("p7")),
+                              tabPanel("Percentage ",plotOutput("p8")),
+                              tabPanel("Plots ",fluidRow( splitLayout(cellWidths = c('50%','50%'),plotOutput("p9"),plotOutput("p10")))),
+                              tabPanel("Tabular",plotOutput("tab2"))),
+        ),
+        tabPanel("Company",
+                 navlistPanel(widths = c(2,10), tabPanel("Plot ",fluidRow( splitLayout(cellWidths = c('50%','50%'),plotOutput("p11"),plotOutput("p12")))),
+                              tabPanel("Tabular",plotOutput("tab3")))
+        ),
+        tabPanel("About",p("Project 3 - Big Yellow Taxi"),p("Data file from the Chicago Data Portal"),
+                 p("An interactive visualization in R and Shiny on Shinyapps.io"),
+                 p("Dashboard initially shows Map and a tabset of plots"),
                  
-                 
-          ),
-        tabPanel("ABOUT", verbatimTextOutput("summary")
-                 
-        )
+                 p("Written by Ariadna Fernandez and Andrea Herrera ") )
         
         
      # )
       
     )
-    
 
-  )
+  ),
+  sidebarPanel(width = '2',
+               
+               # Input: Select the community alphabetically  ----
+               selectInput("community", "Select Community", communities),
+               # br() element to introduce extra vertical spacing ----
+               radioButtons("toFrom", "to/from",
+                            c("starting" = "start",
+                              "ending" = "end"))
+  ) 
 )
 
 # Define server logic for random distribution app ----
@@ -79,11 +94,7 @@ server <- function(input, output) {
   # defined below then use the value computed from this expression
   d <- reactive({
     dist <- switch(input$dist,
-                   norm = rnorm,
-                   unif = runif,
-                   lnorm = rlnorm,
-                   exp = rexp,
-                   rnorm)
+                   norm = rnorm)
     
     dist(input$n)
   })
@@ -94,8 +105,8 @@ server <- function(input, output) {
   # both tracked, and all expressions are called in the sequence
   # implied by the dependency graph.
   output$plot <- renderPlot({
-    dist <- input$dist
-    n <- input$n
+    #dist <- input$dist
+    #n <- input$n
     
     dates <- data.frame(alldata$date)
     colnames(dates) <- c("date")
@@ -109,14 +120,50 @@ server <- function(input, output) {
     #      col = "#75AADB", border = "white")
   })
   
+  output$plotTab <- DT::renderDataTable(
+    DT::datatable({
+      
+      dates <- data.frame(alldata$date)
+      colnames(dates) <- c("date")      
+      
+      dates$r <- format(dates$r, big.mark = ",", scientific = FALSE) 
+      colnames(dates) <- c("Year", "Number of Riders")
+      
+      dates<-as.data.frame(dates)
+      
+    },
+    
+    options = list(searching = FALSE, pageLength = 7, lengthChange = FALSE, order = list(list(0, 'asc'))
+    ), rownames = FALSE
+    )
+  )
+  
   
   output$plot2 <- renderPlot({
-    dist <- input$dist
-    n <- input$n
+    dates <- data.frame(alldata$date)
+    colnames(dates) <- c("date")
     
-    hist(d(),
-         main = paste("r", dist, "(", n, ")", sep = ""),
-         col = "#75AADB", border = "white")
+    ggplot(dates, aes(x=date)) +
+      geom_histogram(binwidth=.5) +
+      geom_density(alpha=.2, fill="#FF6666")
+  })
+  
+  output$plot3 <- renderPlot({
+    dates <- data.frame(alldata$date)
+    colnames(dates) <- c("date")
+    
+    ggplot(dates, aes(x=date)) +
+      geom_histogram(binwidth=.5) +
+      geom_density(alpha=.2, fill="#FF6666")
+  })
+  
+  output$plot4 <- renderPlot({
+    dates <- data.frame(alldata$date)
+    colnames(dates) <- c("date")
+    
+    ggplot(dates, aes(x=date)) +
+      geom_histogram(binwidth=.5) +
+      geom_density(alpha=.2, fill="#FF6666")
   })
   
   # Generate a summary of the data ----
